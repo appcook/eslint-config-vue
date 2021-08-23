e9935793cef4c74f9cb6c900026547929741ac07
Wang Chi <hiwangchi@gmail.com>
2021-08-23 16:39:45 +0800

    v1.1.0 support *.ts?(x)

--- index.js
+++ index.js
@@ -10,11 +10,37 @@ module.exports = {
       experimentalObjectRestSpread: true,
     },
   },
-  rules: {},
   env: {
     browser: true,
     node: true,
     es2021: true,
   },
-  ignorePatterns: ['*.html'],
+  overrides: [
+    {
+      files: '**/*.ts?(x)',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        'prettier',
+      ],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/camelcase': 0,
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/ban-ts-comment': 0,
+      },
+    },
+  ],
 };
